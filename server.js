c5c8d0a5ea30f414c0bc7091c377f11ef70ea93f
Serg <serg27makar@gmail.com>
2021-04-07 12:01:52 +0300

    Refactoring of development and prod modes.

--- server.js
+++ server.js
@@ -7,8 +7,25 @@ const MongoClient = require("mongodb").MongoClient;
 const url = "mongodb://localhost:27017/";
 const mongoClient = new MongoClient(url, { useNewUrlParser: true });
 const server = http.createServer(app);
+var path = require('path');
+var fs = require('fs');
 
 let dbClient;
+
+const route_names = [
+    'users',
+    'store',
+    'product',
+    'parameter'
+];
+
+let api = "../willsi-front/build";
+let usedPort = 80;
+
+if (isDebugging()) {
+    usedPort = 3001;
+    api = "";
+}
 
 app.use(cors());
 
@@ -33,7 +50,7 @@ app.use((request,response,next)=>{
     if (cookies.token && cookies.token.length === 24) {
         next();
     } else {
-        response.sendStatus(401);
+        response.sendStatus(500);
         response.end();
     }
 });
@@ -45,24 +62,73 @@ mongoClient.connect(function(err, client){
     app.locals.products = client.db("willsiDb").collection("productsDb");
     app.locals.store = client.db("willsiDb").collection("storeDb");
     app.locals.parameters = client.db("willsiDb").collection("parametersDb");
-    server.listen(3001, function(){
-        console.log("Сервер ожидает подключения...");
+    server.listen(usedPort, function(){
+        console.log("Server ready...");
     });
 });
-
-const route_names = [
-    'users',
-    'store',
-    'product',
-    'parameter'
-];
 
 route_names.forEach(route_name => {
     let router = require(`./routes/${route_name}`);
     app.use(`/${route_name}`, router);
 });
 
+app.use (function (request, response) {
+    let filePath = '.' + request.url;
+    if (filePath === './')
+        filePath = './index.html';
+
+    const extname = path.extname(filePath);
+    let contentType = 'text/html';
+
+    switch (extname) {
+        case '.js':
+            contentType = 'text/javascript';
+            break;
+        case '.css':
+            contentType = 'text/css';
+            break;
+        case '.json':
+            contentType = 'application/json';
+            break;
+        case '.png':
+            contentType = 'image/png';
+            break;
+        case '.jpg':
+            contentType = 'image/jpg';
+            break;
+        case '.wav':
+            contentType = 'audio/wav';
+            break;
+    }
+
+    filePath = path.join(api, filePath);
+    fs.readFile(filePath, function(error, content) {
+        if (error) {
+            if (error.code === 'ENOENT') {
+                fs.readFile('./404.html', function(error, content) {
+                    response.writeHead(200, { 'Content-Type': contentType });
+                    response.end(content, 'utf-8');
+                });
+            }
+            else {
+                response.writeHead(500);
+                response.end('Sorry, check with the site admin for error: '+error.code+' ..\n');
+                response.end();
+            }
+        }
+        else {
+            response.writeHead(200, { 'Content-Type': contentType });
+            response.end(content, 'utf-8');
+        }
+    });
+})
+
 process.on("SIGINT", () => {
     dbClient.close();
     process.exit();
 });
+
+function isDebugging() {
+    return typeof v8debug === 'object'
+        || /--debug|--inspect/.test(process.execArgv.join(' '));
+}
